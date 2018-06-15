1905753f651bd722e391a3b1233d45a3bff0ec0c
Nelda Suhonen <nelda.suhonen@stud.pointcollege.fi>
2018-06-15 16:18:37 +0300

    Lisätty Tunnit ja Henkilo kontrollerit ja näkymät.

---
+++ TietokantasovellusMvc/TietokantasovellusMvc/Controllers/HenkiloController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TietokantasovellusMvc.Models;
+
+namespace TietokantasovellusMvc.Controllers
+{
+    public class HenkiloController : Controller
+    {
+        private HarjoitustietokantaEntities db = new HarjoitustietokantaEntities();
+
+        // GET: Henkilo
+        public ActionResult Index()
+        {
+
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            List<Henkilot> model = entities.Henkilot.ToList();
+            entities.Dispose();
+
+            return View(model);
+
+            // return View(db.Henkilot.ToList());
+        }
+
+        // GET: Henkilo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Henkilot henkilot = db.Henkilot.Find(id);
+            if (henkilot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(henkilot);
+        }
+
+        // GET: Henkilo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Henkilo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HenkiloID,Etunimi,Sukunimi,Osoite,Esimies")] Henkilot henkilot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Henkilot.Add(henkilot);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(henkilot);
+        }
+
+        // GET: Henkilo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Henkilot henkilot = db.Henkilot.Find(id);
+            if (henkilot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(henkilot);
+        }
+
+        // POST: Henkilo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HenkiloID,Etunimi,Sukunimi,Osoite,Esimies")] Henkilot henkilot)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(henkilot).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(henkilot);
+        }
+
+        // GET: Henkilo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Henkilot henkilot = db.Henkilot.Find(id);
+            if (henkilot == null)
+            {
+                return HttpNotFound();
+            }
+            return View(henkilot);
+        }
+
+        // POST: Henkilo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Henkilot henkilot = db.Henkilot.Find(id);
+            db.Henkilot.Remove(henkilot);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TietokantasovellusMvc/TietokantasovellusMvc/Controllers/TunnitController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TietokantasovellusMvc.Models;
+
+namespace TietokantasovellusMvc.Controllers
+{
+    public class TunnitController : Controller
+    {
+        private HarjoitustietokantaEntities db = new HarjoitustietokantaEntities();
+
+        // GET: Tunnit
+        public ActionResult Index()
+        {
+            HarjoitustietokantaEntities entities = new HarjoitustietokantaEntities();
+            List<Tunnit> model = entities.Tunnit.ToList();
+            entities.Dispose();
+
+            return View(model);
+
+            // return View(db.Tunnit.ToList());
+        }
+
+        // GET: Tunnit/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tunnit tunnit = db.Tunnit.Find(id);
+            if (tunnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tunnit);
+        }
+
+        // GET: Tunnit/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tunnit/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TuntiID,ProjektiID,HenkiloID,Pvm,Tunnit1")] Tunnit tunnit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tunnit.Add(tunnit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tunnit);
+        }
+
+        // GET: Tunnit/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tunnit tunnit = db.Tunnit.Find(id);
+            if (tunnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tunnit);
+        }
+
+        // POST: Tunnit/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TuntiID,ProjektiID,HenkiloID,Pvm,Tunnit1")] Tunnit tunnit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tunnit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tunnit);
+        }
+
+        // GET: Tunnit/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tunnit tunnit = db.Tunnit.Find(id);
+            if (tunnit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tunnit);
+        }
+
+        // POST: Tunnit/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tunnit tunnit = db.Tunnit.Find(id);
+            db.Tunnit.Remove(tunnit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
